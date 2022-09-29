3b1a79a2a77dd4d4fa069515e9d6120b30c59e7f
Avinash Singh <workwith.avinashsingh@gmail.com>
2022-09-29 23:17:48 +0530

    continue

--- src/utility/ESCHideWrapper.jsx
+++ src/utility/ESCHideWrapper.jsx
@@ -1,13 +1,15 @@
-import react, {useEffect} from "react";
+import react, { useEffect } from "react";
 import { useStateContext } from "../contexts/ContextProvider";
 
-const ESCHideWrapper = (props) => {
+const ESCHideWrapper = (props) =>
+{
 
-    const {setThemeSettings , setIsClicked} = useStateContext();
-    const {component} = props
+    const { setThemeSettings, setIsClicked } = useStateContext();
+    const { component } = props;
 
-    const closeOnClick = () => {
-        if(component === "settings")
+    const closeOnClick = () =>
+    {
+        if (component === "settings")
         {
             setThemeSettings(false);
         }
@@ -17,9 +19,11 @@ const ESCHideWrapper = (props) => {
         }
     };
 
-    const handleEscapeKey = (event) => {
-        if (event.code === "Escape") {
-            if(component === "settings")
+    const handleEscapeKey = (event) =>
+    {
+        if (event.code === "Escape") 
+        {
+            if (component === "settings")
             {
                 setThemeSettings(false);
             }
@@ -27,16 +31,17 @@ const ESCHideWrapper = (props) => {
             {
                 setIsClicked(component);
             }
-            
         }
     };
 
-    useEffect(() => {
+    useEffect(() =>
+    {
         document.addEventListener("keydown", handleEscapeKey);
-        
-        return () => {
+
+        return () =>
+        {
             document.removeEventListener("keydown", handleEscapeKey);
-        }
+        };
     }, []);
 
     return (
@@ -44,8 +49,7 @@ const ESCHideWrapper = (props) => {
             <div className="bg-half-transparent w-full h-screen fixed nav-item top-0 right-0" onClick={closeOnClick}></div>
             {props.children}
         </>
-        
     );
-}
+};
 
 export default ESCHideWrapper;
