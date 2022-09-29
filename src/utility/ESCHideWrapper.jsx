1b7cbe39a02f63eba8ed6ed44022db740178795a
Avinash Singh <workwith.avinashsingh@gmail.com>
2022-09-29 22:59:08 +0530

    acting upong code review

--- src/utility/ESCHideWrapper.jsx
+++ src/utility/ESCHideWrapper.jsx
@@ -1,47 +1,47 @@
-import react, {useEffect} from 'react';
+import react, {useEffect} from "react";
 import { useStateContext } from "../contexts/ContextProvider";
-
 
 const ESCHideWrapper = (props) => {
 
     const {setThemeSettings , setIsClicked} = useStateContext();
+    const {component} = props
 
-    const closeOnClick = (e) => {
-        if(props.component === "settings")
+    const closeOnClick = () => {
+        if(component === "settings")
         {
             setThemeSettings(false);
         }
         else
         {
-            setIsClicked(props.component);
+            setIsClicked(component);
         }
     };
 
     const handleEscapeKey = (event) => {
-        if (event.code === 'Escape') {
-            if(props.component === "settings")
+        if (event.code === "Escape") {
+            if(component === "settings")
             {
                 setThemeSettings(false);
             }
             else
             {
-                setIsClicked(props.component);
+                setIsClicked(component);
             }
             
         }
     };
 
     useEffect(() => {
-        document.addEventListener('keydown', handleEscapeKey);
+        document.addEventListener("keydown", handleEscapeKey);
         
         return () => {
-            document.removeEventListener('keydown', handleEscapeKey);
+            document.removeEventListener("keydown", handleEscapeKey);
         }
     }, []);
 
     return (
         <>
-            <div className='bg-half-transparent w-full h-screen fixed nav-item top-0 right-0' onClick={closeOnClick}></div>
+            <div className="bg-half-transparent w-full h-screen fixed nav-item top-0 right-0" onClick={closeOnClick}></div>
             {props.children}
         </>
         
